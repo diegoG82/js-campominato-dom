3354328b525b8074d0d9b5c1eb09335b0a81a1d0
diegoG82 <diegogiardina@hotmail.it>
2023-03-20 16:28:59 +0100

    implementato colore diverso delle celle con bomba

--- js/script.js
+++ js/script.js
@@ -1,12 +1,9 @@
-// Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe.
-// I numeri nella lista delle bombe non possono essere duplicati.
-// [23, 65, 1, 4,78,15,....];
-// In seguito l'utente clicca su una cella: se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina, altrimenti la cella cliccata si colora di azzurro e l'utente può continuare a cliccare sulle altre celle.
-// La partita termina quando il giocatore clicca su una bomba o raggiunge il numero massimo possibile di numeri consentiti.
-// Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha cliccato su una cella che non era una bomba.
-
 // Dichiaro che il numero delle celle sono 100 (gioco base):
 const numberOfsquares = 100;
+
+//   Genero le bombe con la funzione
+const bombs = generatebombs(16, 100);
+console.log(bombs);
 
 // Al click del tasto play generare una griglia di gioco quadrata in cui ogni cella contiene un numero compreso tra 1 e 100.
 
@@ -23,13 +20,14 @@ btnPlay.addEventListener("click", function () {
     const newItem = generateGridItem(i);
     grid.append(newItem);
   }
+
+  //   Definisco la costante bombe
+
+  // In seguito l'utente clicca su una cella: se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina, altrimenti la cella cliccata si colora di azzurro e l'utente può continuare a cliccare sulle altre celle.
 });
 
-// Creo l'array delle 16 bombe nella griglia da 100
-
-const bombs = generatebombs(16, 100);
-
-///////////////////////
+// FUNZIONI
+////////////////////////////////////////////////////////////////
 
 // FUNZIONE PER CREARE IL TESTO
 function generateGridItem(number) {
@@ -37,36 +35,44 @@ function generateGridItem(number) {
   const newSquare = document.createElement("div");
   // AGGIUNGO AL NODO LA CLASSE
   newSquare.classList.add("grid-item");
-  //   creo elemento span con il numero
+  //   CREO ELEMENTO SPAN CON IL NUMERO
   newSquare.innerHTML = `<span>${number}</span>`;
-  // al click aggiungiamo la classe azure
-  newSquare.addEventListener("click", function () {
-    newSquare.classList.add("azure");
-  });
+  // IMPLEMENTO LA FUNZIONE DEL COLORE DELLE CELLE
+  newSquare.addEventListener("click", addColor);
   return newSquare;
 }
 
+// Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe.
+// I numeri nella lista delle bombe non possono essere duplicati.
+
 // FUNZIONE PER GENERARE LE BOMBE
 function generatebombs(numbersquantity, maxNumber) {
-  const numbers = [];
-  while (numbers.length < numbersquantity) {
+  const bombNumbers = [];
+  while (bombNumbers.length < numbersquantity) {
     const rndNumber = getRndInteger(1, maxNumber);
-    if (!numbers.includes(rndNumber)) {
-      numbers.push(rndNumber);
-      console.log(rndNumber);
+    if (!bombNumbers.includes(rndNumber)) {
+      bombNumbers.push(rndNumber);
     }
   }
- 
-}
 
+  return bombNumbers;
+}
 
 // FUNZIONE PER NUMERI RANDOM
-function getRndInteger(min, max){
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+function getRndInteger(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-// FUNZIONE PER COLORARE IL TESTO DI AZZURRO
-function handleItemClick() {
+// FUNZIONE PER IL COLORE DELLE CELLE
+
+function addColor() {
   const clickedNumber = parseInt(this.querySelector("span").textContent);
-  this.classList.add("azure");
+  console.log(clickedNumber);
+
+  if (bombs.includes(clickedNumber)) {
+    this.classList.add("red");
+    alert("Bomba!");
+  } else {
+    this.classList.add("azure");
+  }
 }
